ea6d6e4eb7126e957a9e2c755e62c3c1629de8a1
Justina <justcho5@gmail.com>
2024-03-05 08:11:40 -0800

    halfway through part 6b

---
+++ part6-redux-notes/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import { NewNote } from "./components/NewNote";
+import { Notes } from "./components/Notes";
+import { VisibilityFilter } from "./components/VisibilityFilter";
+
+const App = () => {
+  return (
+    <div>
+      <NewNote />
+      <VisibilityFilter />
+      <Notes />
+    </div>
+  );
+};
+
+export default App;
