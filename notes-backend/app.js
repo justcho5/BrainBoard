e4dd503726badac3cb807fc6166dba15de4b3ef8
Justina <justcho5@gmail.com>
2024-03-27 18:02:43 -0700

    wip: added a testing router

--- notes-backend/app.js
+++ notes-backend/app.js
@@ -31,6 +31,10 @@ app.use(express.json());
 app.use(middleware.requestLogger);
 
 app.use("/api/notes", notesRouter);
+if (process.env.NODE_ENV === "test") {
+  const testingRouter = require("./controllers/testing");
+  app.use("/api/testing", testingRouter);
+}
 app.use("/api/users", usersRouter);
 app.use("/api/login", loginRouter);
 app.use(middleware.unknownEndpoint);
