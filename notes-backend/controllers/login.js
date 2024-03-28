3bac1d1d5cd71137f7a4c1424e134b6981d317aa
Justina <justcho5@gmail.com>
2024-03-27 19:54:33 -0700

    fix login to work when deployed

--- notes-backend/controllers/login.js
+++ notes-backend/controllers/login.js
@@ -17,7 +17,6 @@ loginRouter.post("/", async (request, response) => {
     username: user.username,
     id: user._id,
   };
-
   const token = jwt.sign(userForToken, process.env.SECRET, {
     expiresIn: 60 * 60,
   });
